23889095fde26a90c8dac35b756df5d79c21b96e
Yves Dorfsman <yves@zioup.com>
2019-11-09 15:02:55 -0700

    looping through all cases

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,34 @@
-use std::collections::{HashSet, BTreeSet};
-use std::env;
+use std::collections::{BTreeSet, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader, Seek, SeekFrom};
-use std::process;
 use std::time::Instant;
 
 fn main() -> std::io::Result<()> {
-    //let algos = ["match", "hash", "btree"];
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Need one argument, \"hash\" or \"match\".");
-        process::exit(1);
-    }
+    let funcs: &[&dyn Fn(&mut _) -> (&str, u32)] = &[&with_match, &with_hash, &with_btree];
 
     let f = &File::open("/usr/share/dict/words")?;
     let reader = &mut BufReader::new(f);
     let _load_in_os_fs_cache: Vec<String> = reader.lines().map(|e| e.unwrap()).collect();
     reader.seek(SeekFrom::Start(0))?;
 
-
-    let t0 = Instant::now();
-    let counted = match &args[1][..] {
-        "hash" => Ok(with_hash(reader)),
-        "match" => Ok(with_match(reader)),
-        "btree" => Ok(with_btree(reader)),
-        _ => {
-            eprintln!("You can use only \"hash\", \"match\" or \"btree\"");
-            Err(())
-        }
-    };
-
-    match counted {
-        Ok(c) => println!("{} {:?}", c, t0.elapsed()),
-        Err(_) => eprintln!("Something went wrong ☹️ ."),
+    for algo in funcs.iter() {
+        let t0 = Instant::now();
+        let (desc, counted) = algo(reader);
+        println!("{:?} {} {}", t0.elapsed(), desc, counted);
+        reader.seek(SeekFrom::Start(0))?;
     }
     Ok(())
 }
 
-
-fn with_hash<R: BufRead>(reader: &mut R) -> u32 {
-   let hexash: HashSet<u8> = [
+fn with_hash<R: BufRead>(reader: &mut R) -> (&str, u32) {
+    let description = "one HashSet per word";
+    let hexash: HashSet<u8> = [
         b'A', b'B', b'C', b'D', b'E', b'F', b'a', b'b', b'c', b'd', b'e', b'f',
     ]
     .iter()
     .cloned()
     .collect();
     let mut counter = 0;
-
 
     for line in reader.lines() {
         let word = line.unwrap();
@@ -57,10 +38,11 @@ fn with_hash<R: BufRead>(reader: &mut R) -> u32 {
             counter += 1;
         }
     }
-    counter
+    (description, counter)
 }
 
-fn with_btree<R: BufRead>(reader: &mut R) -> u32 {
+fn with_btree<R: BufRead>(reader: &mut R) -> (&str, u32) {
+    let description = "one btree per word";
     let hexas: BTreeSet<u8> = [
         b'A', b'B', b'C', b'D', b'E', b'F', b'a', b'b', b'c', b'd', b'e', b'f',
     ]
@@ -68,7 +50,6 @@ fn with_btree<R: BufRead>(reader: &mut R) -> u32 {
     .cloned()
     .collect();
     let mut counter = 0;
-
 
     for line in reader.lines() {
         let word = line.unwrap();
@@ -78,14 +59,14 @@ fn with_btree<R: BufRead>(reader: &mut R) -> u32 {
             counter += 1;
         }
     }
-    counter
+    (description, counter)
 }
 
-fn with_match<R: BufRead>(reader: &mut R) -> u32 {
+fn with_match<R: BufRead>(reader: &mut R) -> (&str, u32) {
     // https://dev.to/timclicks/deadbeef-just-say-no-let-s-learn-to-build-a-small-rust-app-to-find-out-what-words-can-you-spell-with-the-letters-a-f-47em
     // https://github.com/timClicks/hexwords/blob/master/src/main.rs
+    let description = "match trick";
     let mut counter = 0;
-
     'lines: for line in reader.lines() {
         let word = line.unwrap();
         for byte in word.bytes() {
@@ -100,6 +81,5 @@ fn with_match<R: BufRead>(reader: &mut R) -> u32 {
             counter += 1;
         }
     }
-    counter
+    (description, counter)
 }
-
